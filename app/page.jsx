4731cbbaa3a444522cad23a84ae5f2180fb06d96
ZABRE Boureima <zabreboureima236@gmail.com>
2024-07-19 15:03:36 +0000

    updatind app/page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         <div className="text-center xl:text-left order-2 xl:order-none">
           <span className="text-xl">Software Developper</span>
           <h1 className="h1 mb-6">
-            Hello I'm <br/> <span className="text-accent">Boureima Zabre</span>
+            Hello I am <br/> <span className="text-accent">Boureima Zabre</span>
           </h1>
           <p className="max-w-[500px] mb-9 text-white/80">I excel at creafting elegant digital experiences and I am proficient in various programming languages and technologies.</p>
           <div className="flex flex-col xl:flex-row items-center gap-8">
