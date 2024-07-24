3c6bf023de57bcecf6c8fd0a981783ad3a0e626d
ZABRE Boureima <zabreboureima236@gmail.com>
2024-07-24 11:59:55 +0000

    updated contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -47,7 +47,7 @@ const Contact = () => {
           {/* form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
             <form action="" className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">Let's work together</h3>
+              <h3 className="text-4xl text-accent">Let is work together</h3>
               <p className="text-white/60">Lorum upsium eu vel elit. mauris at adipiscing auctor ipsum tellus dolor, </p>
               {/* input */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
