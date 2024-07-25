b43db4a87b254dc373f174b5711c193323bf0123
ZABRE Boureima <zabreboureima236@gmail.com>
2024-07-25 13:29:12 +0000

    added cv link mega with alert

--- app/page.jsx
+++ app/page.jsx
@@ -17,13 +17,7 @@ const Home = () => {
           <p className="max-w-[500px] mb-9 text-white/80">I excel at creafting elegant digital experiences and I am proficient in various programming languages and technologies.</p>
           <div className="flex flex-col xl:flex-row items-center gap-8">
           <Link href="https://mega.nz/file/1MlG3bpT#w5rc-HrqtHIrXSZ84E185VwuB3AWfXo515DBZt6t6vE" target="_blank">
-            <Button type="button" variant="outline" size="lg" className="uppercase flex items-center gap-2"
-            onClick={() =>
-              alert(
-                "Vous verrez maintenant un lien Mega.nz. Cliquez sur télécharger pour télécharger le CV au format PDF. Merci pour la visite !!",
-              )
-            }
-            >
+            <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
               <span>Download CV</span>
               <FiDownload className="text-xl" />
             </Button>
