0f4db8fb56f4726c7d5a7c870b528f31ebf94794
ZABRE Boureima <zabreboureima236@gmail.com>
2024-07-26 10:13:56 +0000

    updated contact page info

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -20,7 +20,7 @@ const info = [
   {
     icon: <FaPhoneAlt />,
     title: "Phone",
-    description: "(+226 74352563 / 62694878)",
+    description: "(+226) 74352563 / 62694878",
   },
   {
     icon: <FaEnvelope />,
@@ -78,23 +78,21 @@ const Contact = () => {
           {/* info */}
           
           <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-          <ul className="flex flex-col gap-10">
-            {info.map((item, index)=>{
-              return (  
-                <li key={index} className="flex items-center gap-6">
-                  <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
-                  <div className="text-[28px]">
-                    {item.icon}
-                  </div>
-                  <div className="flex-1">
-                    <p className="text-white/60">{item.title}</p>
-                    <h3 className="text-xl">{item.description}</h3>
-                  </div>
-                </div>
-                </li>
-              )
-            })}
-          </ul>
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index)=>{
+                return (
+                  <li key={index} className="flex items-center gap-6">
+                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.title}</p>
+                      <h3 className="text-xl">{item.description}</h3>
+                    </div>
+                  </li>
+                )
+              })}
+            </ul>
           </div>
         </div>
       </div>
