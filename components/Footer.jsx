a148641257c24de34ed99d58df737cdca5d260c0
ZABRE Boureima <zabreboureima236@gmail.com>
2024-11-08 17:09:03 +0000

    added footer

---
+++ components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+export default function Footer() {
+  return (
+    <footer className="container mx-auto text-[10px]">
+      <div className="flex justify-between items-center mt-12 text-[10px] border-t-2 py-4">
+        <span className="flex">
+          Copyright 2024 &copy; Built by&nbsp;
+          <a href="" target="_blank">
+            YTech
+          </a>
+        </span>
+        <ul className="flex gap-5">
+          <li>Privacy Policy</li>
+          <li>Termes & Conditions</li>
+          <li>Disclaimer</li>
+          <li>Support</li>
+          <li>FAQ</li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
