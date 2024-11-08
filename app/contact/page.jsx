1c07d0b343ef7fcc8bba9f956dbb78c212d23c7c
ZABRE Boureima <zabreboureima236@gmail.com>
2024-11-08 11:59:39 +0000

    updated education descriptif text

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -48,7 +48,7 @@ const Contact = () => {
           <div className="xl:w-[54%] order-2 xl:order-none">
             <form action="" className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-accent">Let is work together</h3>
-              <p className="text-white/60">Let’s work together to bring your ideas to life. Whether you're looking to build a powerful website, optimize your digital strategy, or implement innovative solutions, I’m here to collaborate and help you achieve your goals. Let’s create something amazing together!</p>
+              <p className="text-white/60">Let is work together to bring your ideas to life. Whether you are looking to build a powerful website, optimize your digital strategy, or implement innovative solutions, I m here to collaborate and help you achieve your goals. Let is create something amazing together!</p>
               {/* input */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <Input type="firstname" placeholder="First name"/>
