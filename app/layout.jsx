619ea8ab23fd93c042276688a4b0953f7601f2e2
ZABRE Boureima <zabreboureima236@gmail.com>
2025-01-10 20:51:27 +0000

    footer deleted

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
         <PageTransition>
           {children}
         </PageTransition>
-        <Footer />
+        {/* <Footer /> */}
         </body>
     </html>
   );
