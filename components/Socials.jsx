fcc0fab196090b2271ec74f4f9f9ea472b52cb88
ZABRE Boureima <zabreboureima236@gmail.com>
2025-01-14 09:34:43 +0000

    updated stats

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,22 +1,28 @@
-import Link from "next/link"
-import {FaGithub, FaLinkedinIn, FaTwitter, FaFacebook} from 'react-icons/fa'
-
+import Link from "next/link";
+import { FaGithub, FaLinkedinIn, FaTwitter, FaFacebook } from "react-icons/fa";
 
 const socials = [
-    {icon: <FaGithub />, path:"https://github.com/outis62"},
-    {icon: <FaLinkedinIn />, path:"https://www.linkedin.com/in/boureima-zabre-932aa71b0/"},
-    {icon: <FaFacebook />, path:"https://www.facebook.com/share/19beRUG8fo/"},
-    {icon: <FaTwitter />, path:"https://x.com/boureima_zabre"},
-]
+  { icon: <FaGithub />, path: "https://github.com/outis62" },
+  {
+    icon: <FaLinkedinIn />,
+    path: "https://www.linkedin.com/in/boureima-zabre-932aa71b0/",
+  },
+  { icon: <FaFacebook />, path: "https://www.facebook.com/share/19beRUG8fo/" },
+  { icon: <FaTwitter />, path: "https://x.com/boureima_zabre" },
+];
 
-const Socials = ({containerStyles, iconStyles}) => {
+const Socials = ({ containerStyles, iconStyles }) => {
   return (
     <div className={containerStyles}>
-      {socials.map((item, index)=>{
-        return <Link href={item.path} key={index} className={iconStyles}>{item.icon}</Link>
+      {socials.map((item, index) => {
+        return (
+          <Link href={item.path} key={index} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default Socials
+export default Socials;
--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -1,48 +1,45 @@
-import { animate, motion } from "framer-motion"
-
+import { animate, motion } from "framer-motion";
 
 const stairAnimation = {
-    initial:{
-        top:  "0%",
-    },
-    animate:{
-        top:"100%",
-    },
-    exit:{
-        top:["100%", "0%"],
-    },
+  initial: {
+    top: "0%",
+  },
+  animate: {
+    top: "100%",
+  },
+  exit: {
+    top: ["100%", "0%"],
+  },
 };
 
-
-const reverseIndex = (index) =>{
-    const totalSteps = 8;
-    return totalSteps - index -1;
+const reverseIndex = (index) => {
+  const totalSteps = 8;
+  return totalSteps - index - 1;
 };
 
 const Stairs = () => {
   return (
     <>
-
-  {/* commentaire */}
-  {[...Array(8)].map((_, index)=>{
-    return(
-        <motion.div 
-            key={index} 
-            variants={stairAnimation} 
-            initial="initial" 
-            animate="animate" 
-            exit="exit" 
+      {/* commentaire */}
+      {[...Array(8)].map((_, index) => {
+        return (
+          <motion.div
+            key={index}
+            variants={stairAnimation}
+            initial="initial"
+            animate="animate"
+            exit="exit"
             transition={{
-                duration: 0.4,
-                ease: "easeOut",
-                delay: reverseIndex(index) * 0.1,
-        }} 
-        className="h-full w-full bg-white relative"
-        />
-    )
-  })}
-  </>
+              duration: 0.4,
+              ease: "easeOut",
+              delay: reverseIndex(index) * 0.1,
+            }}
+            className="h-full w-full bg-white relative"
+          />
+        );
+      })}
+    </>
   );
-}
+};
 
-export default Stairs
+export default Stairs;
